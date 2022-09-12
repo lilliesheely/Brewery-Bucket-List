dbea0c5726ee31c68fd80858ed78b7cdc254588d
lilliesheely <lilliesheely@gmail.com>
2022-09-12 11:28:52 -0700

    Fixing issue

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -49,7 +49,7 @@ export default class SignUpForm extends Component {
             <input type="password" name="password" value={this.state.password} onChange={this.handleChange} required />
             <label>Confirm Password</label>
             <input type="password" name="confirm" value={this.state.confirm} onChange={this.handleChange} required />
-            <button type="submit" disabled={disable}>SIGN UP</button>
+            <button type="submit" disabled={disable}>Sign in</button>
           </form>
         </div>
         <p className="error-message">&nbsp;{this.state.error}</p>
